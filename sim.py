6f9a45853e19bec8e9f9a22a11abda40591a86c6
M CPE <mmmiko@hotmail.com>
2022-04-29 00:20:53 +0700

    emacross() + befema() + multiprocessing

--- sim.py
+++ sim.py
@@ -6,75 +6,105 @@ import os
 from binance.client import Client
 import time
 import pandas as pd
+from multiprocessing import Process
 
 def data():
-    global close, closeP, btcusdt
+    global closeP
+    close = []
     candles = client.get_klines(symbol='BTCUSDT', interval=Client.KLINE_INTERVAL_1MINUTE, limit=50)
-    print(len(candles))
+    # print(len(candles))
     for c in candles:
-        close.append(c[4])
+        close.append(float(c[4]))
     closeP = pd.DataFrame({'price': close})
     closeP['ema20'] = closeP['price'].ewm(span=20, adjust=False).mean()
     closeP['ema50'] = closeP['price'].ewm(span=50, adjust=False).mean()
-    print(closeP.iloc[49])
-    btcusdt = client.get_symbol_ticker(symbol='BTCUSDT')
+    # print(closeP.iloc[49])
 
 def buy(amount, wallet):
     wallet["USDT"] -= amount
-    wallet["BTC"] += amount/float(btcusdt['price'])
+    wallet["BTC"] += amount/closeP['price'].iloc[49]
     print("buy")
     return wallet
 
 def sell(amount, wallet):
     wallet["BTC"] -= amount
-    wallet["USDT"] += amount*float(btcusdt['price'])
+    wallet["USDT"] += amount*closeP['price'].iloc[49]
     print("sell")
     return wallet
 
 def emacross():
+    print("emacross START!")
+    count = 0
     while(True):
-        while(closeP['ema20'].iloc[49] > closeP['ema50'].iloc[49] and float(btcusdt['price']) > closeP['ema20'].iloc[49]):
-            print("s")
-            data()
-            if closeP['ema20'].iloc[49] < closeP['ema50'].iloc[49]:
-                print("sell")
-                print(sell(0.01, walletEMA))
-                break
-            time.sleep(10)
-        while(closeP['ema20'].iloc[49] < closeP['ema50'].iloc[49] and float(btcusdt['price']) < closeP['ema20'].iloc[49]):
-            print("b")
-            data()
-            if closeP['ema20'].iloc[49] > closeP['ema50'].iloc[49]:
-                print("buy")
-                print(buy(10, walletEMA))
-                break
-            time.sleep(10)
-            # print(buy(10, walletEMA))
 
-# def befema():
+        # while(closeP['price'].iloc[49] > closeP['ema50'].iloc[49]):
+            # print("bullish1")
+        if closeP['ema20'].iloc[49] > closeP['ema50'].iloc[49] and count == 0:
+            print(buy(10, walletEMA))
+            count = 1
+                # break
+            # data()
+            # time.sleep(10)
+
+        # while(closeP['price'].iloc[49] < closeP['ema50'].iloc[49]):
+            # print("bearish1")
+        if closeP['ema20'].iloc[49] < closeP['ema50'].iloc[49] and count == 1:
+            print(sell(0.01, walletEMA))
+            count = 0
+                # break
+            # data()
+            # time.sleep(10)
+
+        data()
+        time.sleep(10)
+
+def befema():
+    print("befema START!")
+    while(True):
+
+        while(closeP['ema20'].iloc[49] < closeP['ema50'].iloc[49]):
+            print("bearish2")
+            if closeP['price'].iloc[49] > closeP['ema50'].iloc[49]:
+                print(buy(10, walletbef))
+                break
+            data()
+            time.sleep(10)
+
+        count = 0
+        while(closeP['ema20'].iloc[49] > closeP['ema50'].iloc[49]):
+            print("bullish2")
+            if closeP['price'].iloc[49] < closeP['ema20'].iloc[49] and count == 0:
+                print(sell(0.005, walletbef))
+                count = 1
+            if closeP['price'].iloc[49] < closeP['ema50'].iloc[49] and count == 1:
+                print(sell(0.005, walletbef))
+                break
+            data()
+            time.sleep(10)
+
+        data()
+        time.sleep(10)
 
 api_key = os.environ.get('binance_api')
 api_secret = os.environ.get('binance_secret')
 
 client = Client(api_key, api_secret)
 
-# while(True):
-#     time.sleep(10)
-#     btcusdt = client.get_symbol_ticker(symbol='BTCUSDT')
-    # print(btcusdt)
-
 close = []
-walletEMA = {"USDT": 1000, "BTC": 1}
-walletbef = {"USDT": 1000, "BTC": 1}
+walletEMA = {"name": "emacross", "USDT": 1000, "BTC": 1}
+walletbef = {"name": "befema", "USDT": 1000, "BTC": 1}
 closeP = pd.DataFrame
-btcusdt = {}
-# print(btcusdt['price'])
-# print(client.get_exchange_info())
-
-# print(buy(10))
-# print(sell(0.00005))
-# print(closeP['ema20'].iloc[49])
 
 data()
 
-emacross()
+# emacross()
+
+if __name__ == '__main__':
+    p1 = Process(target=emacross)
+    p2 = Process(target=befema)
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
